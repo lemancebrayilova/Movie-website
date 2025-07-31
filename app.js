9cb914a4e7fd47eb4a98e7c5a72f926b51027042
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-07-31 17:25:25 +0300

    Add video play/pause toggle and filter cards by type (series/movies)

--- app.js
+++ app.js
@@ -84,4 +84,80 @@ fetch(json_url).then(Response => Response.json())
                 }
             }
         })
+
+        let video = document.getElementsByTagName('video')[0];
+        let play = document.getElementById('play');
+
+        play.addEventListener('click', () => {
+            if (video.paused) {
+                video.play();
+                play.innerHTML = `Play <i class="bi bi-pause-fill"></i>`
+            } else {
+                play.innerHTML = `Watch <i class="bi bi-play-fill"></i>`
+                video.pause();
+            }
+        })
+
+        let series = document.getElementById('series');
+        let movies = document.getElementById('movies');
+
+        series.addEventListener('click', () => {
+            cards.innerHTML = '';
+
+            let series_array = data.filter(ele => {
+                return ele.type === "series";
+            });
+
+            series_array.forEach((ele, i) => {
+                let {name, imdb, date, sposter, bposter, genre, url } = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = `
+                <img src="${sposter}" alt="${name}" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                        </div>
+                    </div>
+                </div>
+                `;
+
+                cards.appendChild(card);
+            });
+        })
+
+        movies.addEventListener('click', () => {
+            cards.innerHTML = '';
+
+            let movie_array = data.filter(ele => {
+                return ele.type === "movie";
+            });
+
+            movie_array.forEach((ele, i) => {
+                let {name, imdb, date, sposter, bposter, genre, url } = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = `
+                <img src="${sposter}" alt="${name}" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill"></i> ${imdb}</h3>
+                        </div>
+                    </div>
+                </div>
+                `;
+
+                cards.appendChild(card);
+            });
+        })
     });
